7d2ce57773c567dc8999fe87d98a90e361c9ce53
Jérémi Le Bourhis <jeremi.leb@gmail.com>
2018-03-22 14:08:56 +0100

    downgrade directive edit joke

--- src/app-ng1.js
+++ src/app-ng1.js
@@ -2,12 +2,14 @@ import * as angular from 'angular';
 
 import HomeModule from './app/home/home.module';
 import CoreModule from './app/core/core.module.js';
+import JokeModule from './app/joke/joke.module.js';
 
 export default angular.module('App', [
   'ui.router',
   'ui.router.upgrade',
   HomeModule,
   CoreModule,
+  JokeModule,
 ])
   .config(config)
   .name;
--- src/app/joke/joke.module.ts
+++ src/app/joke/joke.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { UIRouterModule } from '@uirouter/angular';
+import { downgradeComponent } from '@angular/upgrade/static';
 
 import { CoreModule } from '../core/core.module';
 
@@ -10,6 +11,12 @@ import { AddJokeComponent } from './add-joke/add-joke.component';
 import { EditJokeComponent } from './edit-joke/edit-joke.component';
 
 import { states } from './joke.states';
+
+import * as angular from 'angular';
+import JokeModuleNG1 from './joke.module.js';
+
+angular.module(JokeModuleNG1)
+  .directive('editJoke', downgradeComponent({ component: EditJokeComponent }));
 
 
 @NgModule({
@@ -19,6 +26,7 @@ import { states } from './joke.states';
   ],
   entryComponents: [
     AddJokeComponent,
+    EditJokeComponent,
   ],
   imports: [
     HttpClientModule,
